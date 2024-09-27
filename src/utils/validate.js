93a55a62021bcf7de24653746c8a3b8577cf66fe
aamir2401 <aamir.anwar2401@gmail.com>
2024-09-28 00:02:16 +0530

    Validate

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,18 +1,18 @@
-export const formValidator = (email, password, mobileNumber, fullName) =>
+export const formValidator = (email, password, fullName) =>
 {
      const isValidEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
      const isValidPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
-     const isValidMobNumber = /^[6-9]\d{9}$/.test(mobileNumber)
+     // const isValidMobNumber = /^[6-9]\d{9}$/.test(mobileNumber)
      const isValidFullName = /^[a-zA-Z]+([ '-][a-zA-Z]+)*$/.test(fullName)
 
 
 
      if(!isValidEmail)
-     return "Invalid Email!!!"
+     return "Invalid Email!!"
      if(!isValidPassword)
-     return "Invalid Password!!!"
-     if(!isValidMobNumber)
-     return "Invalid Mobile Number Format"
+     return "Invalid Password!!"
+     // if(!isValidMobNumber)
+     // return "Invalid Mobile Number Format.!!"
      if(!isValidFullName)
-     return "Invalid Name Format"
+     return "Invalid Name Format!!"
 }
